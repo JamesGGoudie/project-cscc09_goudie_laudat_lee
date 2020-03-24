79615af57feb58f32feddef8d0ce14bc0d4236ef
James Gabriel Goudie <james.g.goudie@gmail.com>
2020-03-24 19:53:58 -0400

    Added 'proxied' to rtc server.

--- web-rtc/server.js
+++ web-rtc/server.js
@@ -1,2 +1,6 @@
 const { PeerServer } = require('peer');
-const ps = PeerServer({port: 9000, path: '/peer', key: 'architect'});
+const ps = PeerServer({
+    port: 9000,
+    path: '/peer',
+    key: 'architect',
+    proxied: true});
