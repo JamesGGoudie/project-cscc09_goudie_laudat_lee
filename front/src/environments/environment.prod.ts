59128b55408db0f8fed4e0ed6faa997faa26d4a6
James Gabriel Goudie <james.g.goudie@gmail.com>
2020-03-24 20:12:14 -0400

    Reset peer-js to default when on prod.

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   backend: '',
-  peerHost: '/',
-  peerKey: 'architect',
-  peerPath: 'peer',
-  peerPort: 9000,
+  peerHost: '0.peerjs.com',
+  peerKey: 'peerjs',
+  peerPath: '/',
+  peerPort: 80,
   production: true
 };
