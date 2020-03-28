1d7378c3b344fd569fa97e26ff359375ebdfd515
James Gabriel Goudie <james.g.goudie@gmail.com>
2020-03-28 18:51:57 -0400

    Moved the graphql functions to a dedicated file.

--- back/src/app.ts
+++ back/src/app.ts
@@ -4,14 +4,7 @@ import express from 'express';
 import graphqlHTTP from 'express-graphql';
 
 import { GQL_SCHEMA } from './constants';
-
-import {
-  CreateWorkspaceReq,
-  CreateWorkspaceRes,
-  JoinWorkspaceReq,
-  JoinWorkspaceRes
-} from './interfaces';
-
+import { GraphQlFactory } from './graphql';
 import { Database } from './services';
 
 // const FRONT = 'http://localhost:4200';
@@ -29,89 +22,13 @@ app.use((req, res, next) => {
   next();
 });
 
-const db = new Database();
+const db: Database = new Database();
+const factory: GraphQlFactory = new GraphQlFactory(db);
 
 db.connectDatabase().then(() => {
-  const root = {
-    createWorkspace: async (
-      req: CreateWorkspaceReq
-    ): Promise<CreateWorkspaceRes> => {
-      return await db.workspaceExists(req.workspaceId).then(
-          (workspaceExists: boolean): Promise<boolean> => {
-        if (workspaceExists) {
-          throw 'Workspace already exists';
-        }
-
-        return db.createWorkspace(req.workspaceId, req.workspacePassword);
-      }).then((created: boolean): Promise<string> => {
-        if (!created) {
-          throw 'Workspace does not exist, but failed to create workspace';
-        }
-
-        return db.addUserToWorkspace(req.workspaceId, req.userId);
-      }).then((peerId: string): CreateWorkspaceRes => {
-        if (!peerId) {
-          throw 'Workspace created, but user was not';
-        }
-
-        return {
-          yourPeerId: peerId
-        }
-      }).catch((err: string): CreateWorkspaceRes => {
-        return {err};
-      });
-    },
-    joinWorkspace: async (
-      req: JoinWorkspaceReq
-    ): Promise<JoinWorkspaceRes> => {
-      let peerId: string;
-
-      return await db.workspaceExists(req.workspaceId).then(
-          (workspaceExists: boolean): Promise<boolean> => {
-        if (!workspaceExists) {
-          throw 'Workspace does not exist';
-        }
-
-        return db.userExists(req.workspaceId, req.userId);
-      }).then((userExists: boolean): Promise<boolean> => {
-        if (userExists) {
-          throw 'Username is taken';
-        }
-
-        return db.passwordMatches(req.workspaceId, req.workspacePassword);
-      }).then((matches: boolean): Promise<string> => {
-        if (!matches) {
-          throw 'Wrong password';
-        }
-
-        return db.addUserToWorkspace(req.workspaceId, req.userId);
-      }).then((peer: string): Promise<string[]> => {
-        if (!peer) {
-          throw 'Workspace exists, password is correct, and user ID is' +
-              ' available, but failed to add user to workspace';
-        }
-
-        peerId = peer;
-
-        return db.getOtherUsersPeerIds(req.workspaceId, req.userId);
-      }).then((otherPeers: string[]): JoinWorkspaceRes => {
-        if (!otherPeers) {
-          throw 'User was added to workspace, but could not get other users';
-        }
-
-        return {
-          otherPeerIds: otherPeers,
-          yourPeerId: peerId
-        }
-      }).catch((err: string): JoinWorkspaceRes => {
-        return {err}
-      });
-    }
-  }
-
   const graphQlOptions: graphqlHTTP.Options = {
     graphiql: true,
-    rootValue: root,
+    rootValue: factory.getRoot(),
     schema: GQL_SCHEMA
   }
 
---
+++ back/src/graphql/graphql-factory.ts
@@ -0,0 +1,98 @@
+import {
+  CreateWorkspaceReq,
+  CreateWorkspaceRes,
+  GraphQlRoot,
+  JoinWorkspaceReq,
+  JoinWorkspaceRes
+} from '../interfaces';
+
+import { Database } from '../services';
+
+export class GraphQlFactory {
+
+  public constructor(private readonly db: Database) {}
+
+  private readonly createWorkspace:
+      (req: CreateWorkspaceReq)=> Promise<CreateWorkspaceRes> =
+      async (req: CreateWorkspaceReq): Promise<CreateWorkspaceRes> => {
+    return await this.db.workspaceExists(req.workspaceId).then(
+        (workspaceExists: boolean): Promise<boolean> => {
+      if (workspaceExists) {
+        throw 'Workspace already exists';
+      }
+
+      return this.db.createWorkspace(req.workspaceId, req.workspacePassword);
+    }).then((created: boolean): Promise<string> => {
+      if (!created) {
+        throw 'Workspace does not exist, but failed to create workspace';
+      }
+
+      return this.db.addUserToWorkspace(req.workspaceId, req.userId);
+    }).then((peerId: string): CreateWorkspaceRes => {
+      if (!peerId) {
+        throw 'Workspace created, but user was not';
+      }
+
+      return {
+        yourPeerId: peerId
+      }
+    }).catch((err: string): CreateWorkspaceRes => {
+      return {err};
+    });
+  };
+
+  private readonly joinWorkspace:
+      (req: JoinWorkspaceReq)=> Promise<JoinWorkspaceRes> =
+      async (req: JoinWorkspaceReq): Promise<JoinWorkspaceRes> => {
+    let peerId: string;
+
+    return await this.db.workspaceExists(req.workspaceId).then(
+        (workspaceExists: boolean): Promise<boolean> => {
+      if (!workspaceExists) {
+        throw 'Workspace does not exist';
+      }
+
+      return this.db.userExists(req.workspaceId, req.userId);
+    }).then((userExists: boolean): Promise<boolean> => {
+      if (userExists) {
+        throw 'Username is taken';
+      }
+
+      return this.db.passwordMatches(req.workspaceId, req.workspacePassword);
+    }).then((matches: boolean): Promise<string> => {
+      if (!matches) {
+        throw 'Wrong password';
+      }
+
+      return this.db.addUserToWorkspace(req.workspaceId, req.userId);
+    }).then((peer: string): Promise<string[]> => {
+      if (!peer) {
+        throw 'Workspace exists, password is correct, and user ID is' +
+            ' available, but failed to add user to workspace';
+      }
+
+      peerId = peer;
+
+      return this.db.getOtherUsersPeerIds(req.workspaceId, req.userId);
+    }).then((otherPeers: string[]): JoinWorkspaceRes => {
+      if (!otherPeers) {
+        throw 'User was added to workspace, but could not get other users';
+      }
+
+      return {
+        otherPeerIds: otherPeers,
+        yourPeerId: peerId
+      }
+    }).catch((err: string): JoinWorkspaceRes => {
+      return {err}
+    });
+  }
+
+  public getRoot(): GraphQlRoot {
+    return {
+      createWorkspace: this.createWorkspace,
+      joinWorkspace: this.joinWorkspace
+    };
+  }
+
+}
