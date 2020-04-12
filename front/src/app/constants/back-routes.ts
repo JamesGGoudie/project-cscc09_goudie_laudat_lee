54fd59a0e375fe828d8ad891242cac9f36249736
James Gabriel Goudie <james.g.goudie@gmail.com>
2020-04-12 04:17:36 -0400

    Fixed a spelling mistake.

--- front/src/app/constants/back-routes.ts
+++ front/src/app/constants/back-routes.ts
@@ -1,7 +1,7 @@
 import { environment } from 'src/environments/environment';
 
 /**
- * Notable routes to the backend.
+ * Noteable routes to the backend.
  */
 export const BACK_ROUTES = {
   API: environment.backend + '/graphql'
